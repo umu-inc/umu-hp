5fbd78ef4c461d228c732de5947e6b2617e26769
Murakami7777@ <nn200494@gmail.com>
2020-12-21 12:39:41 +0900

    add_some

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -4,10 +4,24 @@ import { graphql } from 'gatsby'
 import PageHeader from '../components/PageHeader'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
+import styled from 'styled-components';
+
+const Text = styled.div`
+padding: 0.5em 1em;
+margin: 2em 5em;
+color: #232323;
+background: #F5F5F5;
+> p {
+  margin-top: 1em; 
+  padding: 0;
+  }
+`
 
 
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
+
+  
   <main className="Home">
     <PageHeader
       large
@@ -19,11 +33,11 @@ export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
     <section className="section">
       <div className="container">
         <Content source={body} />
-        <div>
-          <p>エンジニアの普通これくらい分かるだろうは世間には通用しない！</p>
-          <p>私たちUmuは目線を普通の生活者に寄せ、誰にでも分かりやすいデザインを提供することを心がけています。</p>
-        </div>
       </div>
+      <Text>
+        <p>エンジニアの普通これくらい分かるだろうは世間には通用しない！<br/>
+        私たちUmuは目線を普通の生活者に寄せ、誰にでも分かりやすいデザインを提供することを心がけています。</p>
+      </Text>
     </section>
   </main>
 )
